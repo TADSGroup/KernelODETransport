6f6d88e87fb25fe501c60905825d02509e12fb91
birajpandey <bpandey@uw.edu>
2024-03-20 13:54:20 -0700

    Set up directory structure and requirements.
    - Used CookieCutter Datascience to set up directory structure.
    - Set up minimal requirements for the project.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='kode',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We present a general framework for the transport of probability measures towards minimum divergence generative modeling and sampling using ordinary differential equations (ODEs) and Reproducing Kernel Hilbert Spaces (RKHSs).',
+    author='Biraj Pandey',
+    license='MIT',
+)
