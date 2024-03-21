d5c40c3437f7e282d825564dfd9052a3f1957567
birajpandey <bpandey@uw.edu>
2024-03-21 13:14:21 -0700

    Updated setup.sh to correctly install proper JAX/Pytorch packages based on Apple M1/Linux systems.
    JAX is still experimental and diffrax doesn't really work on M1. :(

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# Define global variable for environment name
+# Define global variables
 ENV_NAME="kode_env_test"
+OS_NAME="$(uname -s)"
+HARDWARE_NAME="$(uname -m)"
+
+echo "Detected OS: $OS_NAME, Hardware: $HARDWARE_NAME."
 
 # Function to check if Conda is installed
 check_conda(){
   if ! command -V conda &> /dev/null; then
-    echo "Conda could not be found. Please install conda before continuing."
+    echo "Conda could not be found. Please install Anaconda before
+    continuing. https://www.anaconda.com/download"
     exit 1
 
   fi
@@ -15,7 +20,7 @@ check_conda(){
 
 # Creating a new Conda environment and activating it
 create_conda_env(){
-  echo "Creating a new Conda environment..."
+  echo "\n Creating a new Conda environment... \n"
   conda create --name "$ENV_NAME" python=3.8 -y
 }
 
@@ -23,31 +28,43 @@ create_conda_env(){
 # Global variable for conda
 ENV_BIN_PATH="$(conda info --base)/envs/$ENV_NAME/bin"
 
+
 # Installing Python packages from requirements.txt
 install_requirements(){
-  echo "Installing requirements from requirements.txt in the conda
-  environment $ENV_NAME..."
+  echo "\n Installing requirements from requirements.txt in the conda
+  environment $ENV_NAME... \n"
   "$ENV_BIN_PATH/pip" install -r requirements.txt
 }
 
-# Installing JAX based on CPU/GPU and cuda versions
+
+# Installing Pytorch based on Apple M1/Linux
+install_pytorch(){
+
+  # Default installation command
+  local torch_install_command="pip install torch torchvision"
+
+    if [[ "$OS_NAME" == "Darwin" && "$HARDWARE_NAME" == "arm64" ]]; then
+      torch_install_command="pip install --pre torch torchvision torchaudio
+      --extra-index-url https://download.pytorch.org/whl/nightly/cpu"
+
+    fi
+  echo "\n Running TORCH install command: $ENV_BIN_PATH/$torch_install_command"
+  eval "$ENV_BIN_PATH/$torch_install_command"
+}
+
+
+# Installing JAX based on Apple M1/Linux and CPU/GPU
 install_jax(){
-
-  # Identify the operating system and machine hardware name
-  local os_name="$(uname -s)"
-  local hw_name="$(uname -m)"
-
-  echo "Detected OS: $os_name, Hardware: $hw_name"
 
   # Default to CPU Linux installation
   local jax_install_command="pip install --upgrade 'jax[cpu]'"
 
   # Check if Apple Silicon or Linux
-  if [[ "$os_name" == "Darwin" && "$hw_name" == "arm64" ]]; then
+  if [[ "$OS_NAME" == "Darwin" && "$HARDWARE_NAME" == "arm64" ]]; then
     echo "Detected Apple MacBook Silicon M1"
     jax_install_command="pip install jax-metal"
 
-  elif [[ "$os_name" == "Linux" ]]; then
+  elif [[ "$OS_NAME" == "Linux" ]]; then
     echo "Detected a Linux system"
     # Linux with or without GPU
 
@@ -76,14 +93,14 @@ install_jax(){
     fi
   fi
 
-  echo "Running JAX install command: $ENV_BIN_PATH/$jax_install_command"
+  echo "\n Running JAX install command: $ENV_BIN_PATH/$jax_install_command \n"
   eval "$ENV_BIN_PATH/$jax_install_command"
   }
 
 
 # Install Diffrax and optax
 install_diffrax(){
-  echo "Installing Diffrax for solving ODEs in JAX..."
+  echo "\n Installing Diffrax for solving ODEs in JAX... \n"
   eval "$ENV_BIN_PATH/pip install diffrax optax"
 }
 
@@ -93,6 +110,7 @@ main(){
   check_conda
   create_conda_env
   install_requirements
+  install_pytorch
   install_jax
   install_diffrax
   echo "Setup complete."
